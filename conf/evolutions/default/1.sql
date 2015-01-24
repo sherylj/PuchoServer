a4de1afc61016bfe5aaf1f5f38a5baee9635fc98
sherylj <sherylj@usc.edu>
2015-01-24 01:18:59 -0800

    New evolutions sql

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -4,31 +4,33 @@
 # --- !Ups
 
 create table area (
-  id                        bigint auto_increment not null,
+  id                        bigint not null,
   name                      varchar(255),
   constraint pk_area primary key (id))
 ;
 
 create table country (
-  id                        bigint auto_increment not null,
+  id                        bigint not null,
   name                      varchar(255),
   constraint pk_country primary key (id))
 ;
 
 create table language (
-  id                        bigint auto_increment not null,
+  id                        bigint not null,
   name                      varchar(255),
   constraint pk_language primary key (id))
 ;
 
 create table user (
-  id                        bigint auto_increment not null,
+  id                        bigint not null,
   firstname                 varchar(255),
   lastname                  varchar(255),
   profession                varchar(255),
+  auth_token                varchar(255),
   username                  varchar(255),
   email                     varchar(255),
   password                  varchar(255),
+  sha_password              varbinary(255),
   linkedin                  varchar(255),
   personal_url              varchar(255),
   country_id                bigint,
@@ -36,15 +38,25 @@ create table user (
 ;
 
 create table user_education (
-  id                        integer auto_increment not null,
+  id                        integer not null,
   user_id                   bigint,
   degree                    varchar(255),
   institution               varchar(255),
-  start_year                datetime,
-  end_year                  datetime,
+  start_year                timestamp,
+  end_year                  timestamp,
   area_id                   bigint,
   constraint pk_user_education primary key (id))
 ;
+
+create sequence area_seq;
+
+create sequence country_seq;
+
+create sequence language_seq;
+
+create sequence user_seq;
+
+create sequence user_education_seq;
 
 alter table user add constraint fk_user_country_1 foreign key (country_id) references country (id) on delete restrict on update restrict;
 create index ix_user_country_1 on user (country_id);
@@ -57,17 +69,27 @@ create index ix_user_education_area_3 on user_education (area_id);
 
 # --- !Downs
 
-SET FOREIGN_KEY_CHECKS=0;
+SET REFERENTIAL_INTEGRITY FALSE;
 
-drop table area;
+drop table if exists area;
 
-drop table country;
+drop table if exists country;
 
-drop table language;
+drop table if exists language;
 
-drop table user;
+drop table if exists user;
 
-drop table user_education;
+drop table if exists user_education;
 
-SET FOREIGN_KEY_CHECKS=1;
+SET REFERENTIAL_INTEGRITY TRUE;
+
+drop sequence if exists area_seq;
+
+drop sequence if exists country_seq;
+
+drop sequence if exists language_seq;
+
+drop sequence if exists user_seq;
+
+drop sequence if exists user_education_seq;
 
